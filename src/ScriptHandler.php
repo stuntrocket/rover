60e6394c58d363e7a5e2de93d49d6f9e12ca1ce6
DannyHarding <danny@stuntrocket.co>
2023-10-23 01:08:12 +0100

    Update symlink creation in ScriptHandler
    Updated the `createSymlink` function in `ScriptHandler`. The method now considers two possible locations of the `robo` executable - top-level and nested vendor directories. This is to ensure the successful creation of the symbolic link regardless of the installation type of the 'consolidation/robo' package. Also added a check and creation of destination directory if it does not exist.

--- src/ScriptHandler.php
+++ src/ScriptHandler.php
@@ -11,8 +11,21 @@ class ScriptHandler
     public static function createSymlink(): void
     {
         try {
-            $roboSrc = InstalledVersions::getInstallPath('consolidation/robo') . '/robo';
-            $roboDest = __DIR__ . '/../vendor/bin/robo';
+            // Check the top-level vendor directory first
+            $roboSrcTopLevel = __DIR__ . '/../../../vendor/consolidation/robo/robo';
+            // Check the nested vendor directory second
+            $roboSrcNested = InstalledVersions::getInstallPath('consolidation/robo') . '/robo';
+
+            // Determine the actual source path based on the location of the robo binary
+            $roboSrc = file_exists($roboSrcTopLevel) ? $roboSrcTopLevel : $roboSrcNested;
+
+            // Destination path remains the same
+            $roboDestDir = __DIR__ . '/../../../vendor/bin';
+            $roboDest = $roboDestDir . '/robo';
+
+            if (!file_exists($roboDestDir)) {
+                mkdir($roboDestDir, 0755, true);
+            }
 
             if (file_exists($roboDest)) {
                 throw new Exception("A file or symlink already exists at {$roboDest}. Aborting to prevent overwriting.");
